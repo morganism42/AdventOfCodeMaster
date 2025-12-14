458e4233df120f12f60cdb2d77f416abe702e29e
Liam Stregger <liamstregger@gmail.com>
2025-12-14 13:26:09 -0800

    initial commit

--- 2015/dec21.py
+++ 2015/dec21.py
@@ -1,23 +1,19 @@
 from aocd import get_data, submit
+
+
 def solve1(test=True):
-    # Your solution code here
-    testdata = ''''''
-    testans = ''
-    if test:
+	# Your solution code here
+	testdata = ''''''
+	testans = ''
+	if test:
 		data = testdata
 	else:
 		data = get_data(day=21, year=2015)
-    
-    
-    
-    
-    
-    
-    if test and result == testans:
-    	print("Test passed")
-    elif test and result != testans:
+
+	if test and result == testans:
+		print("Test passed")
+	elif test and result != testans:
 		print("Test failed")
 	elif not test:
 		print(result)
-        submit(result, part="a", day=21, year=2015)
-
+	submit(result, part="a", day=21, year=2015)
